cdb7c6cc5e7f4750344796eb006b61b2ca6c8659
Xinrui Du <xinruidu02@outlook.com>
2025-12-19 19:15:57 +0100

    Add files via upload

---
+++ simulation.R
@@ -0,0 +1,27 @@
+
+source("simulation_functions.R")
+
+N <- c(200, 500, 1000, 2000)
+G <- list()
+
+for (sample in N) {
+  G[[sample]] <- GenerateGraph(n_parameters = 10,
+                     sparsity = 0.29,
+                     theta = 0.35,
+                     N = sample,
+                     seed = 67,
+                     verbose=FALSE,
+                     max_check=10000)
+  G_check <- apply(G[[sample]]$G, 3, function(x) {
+    eig_x <- eigen(x)
+    sum(abs(eig_x$values) >= 1) > 0 # TRUE if violated
+  })
+  print(sum(G_check))
+}
+
+Data <- list()
+for (sample in N) {
+  Data[[sample]] <- Data1000 <- VARData(G[[sample]]$G, matrix(0, sample, 10), 1, seed = 67)
+}
+
+save(Data, G, N, file = "Data.Rdata")
